14649dcd1b694a9fb93386a76d027a7e2a7f74ae
Mctks2 <mctks2@hotmail.com>
2024-11-19 11:27:02 -0300

    aula 05

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
@@ -15,16 +15,9 @@ export class LivroService {
   // O HttpClient será usado para realizar as requisições HTTP.
   constructor(private http: HttpClient) { }
 
-  buscar(valorDigitado: string): Observable<Item[]> {
-    //Cria um objeto de parâmetros para a requisição HTTP.
-    // Aqui, o parâmetro 'q' é adicionado com o valor que o usuário digitou.
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado )
 
-    // Retorna um Observable que emitirá os dados da resposta quando a requisição for concluída.
-    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
-      tap((retornoAPI) => console.log('Fluxo do tap', retornoAPI)),
-      map(resultado => resultado.items),
-      tap(resultado => console.log('Fluxo do map', resultado))
-    )
+    return this.http.get<LivrosResultado>(this.API, { params })
   }
 }
